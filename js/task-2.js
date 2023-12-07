f360f38767988b80b7b1ab26065b3a1090c75814
Rick <46992687+rick-77-2006@users.noreply.github.com>
2023-12-07 09:44:45 +0200

    111

---
+++ js/task-2.js
@@ -0,0 +1,6 @@
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
