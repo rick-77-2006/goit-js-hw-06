c1bd5745ebe9d9b6ac211a2bf79e4dbf82adad01
Rick <46992687+rick-77-2006@users.noreply.github.com>
2023-12-14 13:40:19 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,22 +1,17 @@
 class StringBuilder {
   #value;
-
   constructor(initialValue) {
     this.#value = initialValue;
   }
-
   getValue() {
     return this.#value;
   }
-
   padEnd(str) {
     this.#value = this.#value + str;
   }
-
   padStart(str) {
     this.#value = str + this.#value;
   }
-
   padBoth(str) {
     this.#value = str + this.#value + str;
   }
